20ecb1b67d3a409229f6be0c7f0bc311ff6974b5
vasavi666 <vasavipudi666@gmail.com>
2024-12-13 22:44:57 +0530

    five.commit

---
+++ day-app/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import React from 'react';
+
+function App() {
+  return (
+  
+    <div className="App">
+     const items = ['Apple', 'Banana', 'Orange', 'Mango'];
+    </div>
+  );
+}
+
+export default App;
---
+++ new-app/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>hii reddy...this is vasavi</h1>
+    </div>
+  );
+}
+
+export default App;
